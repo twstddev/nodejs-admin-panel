54ce45bf38cdedf32a347a51104d245fa5cc7f3a
twstddev <twstd.dev@gmail.com>
2014-06-23 15:36:12 +0000

    Add initial development application

---
+++ app/application.js
@@ -0,0 +1,9 @@
+/**
+ * @brief This is a starting file that implements
+ * application instance.
+ */
+var express = require( "express" );
+
+var application = express();
+
+module.exports = application;
