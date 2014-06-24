7890527b0ece6c1a16036a252f3316ef757a1510
twstddev <twstd.dev@gmail.com>
2014-06-24 14:49:32 +0000

    Add user model test

---
+++ test/spec/include/models/user_spec.js
@@ -0,0 +1,27 @@
+var expect = require( "chai" ).expect;
+
+describe( "User", function() {
+	it( "requires username", function() {
+	} );
+
+	it( "requires username to be unique", function() {
+	} );
+
+	it( "requires password", function() {
+	} );
+
+	it( "requires password confirmation", function() {
+	} );
+
+	it( "requires email", function() {
+	} );
+
+	it( "requires email to be unique", function() {
+	} );
+
+	it( "validates email", function() {
+	} );
+
+	it( "checks if a given password is valid", function() {
+	} );
+} );
