e01311f60d4dbfa1a6001aa8c58250ef3505b9a2
twstddev <twstd.dev@gmail.com>
2014-06-25 14:00:52 +0000

    Add mocha mongoose

--- test/spec/include/models/user_spec.js
+++ test/spec/include/models/user_spec.js
@@ -1,23 +1,14 @@
 describe( "User", function() {
-	it( "requires username", function() {
-	} );
-
-	it( "requires username to be unique", function() {
-	} );
-
-	it( "requires password", function() {
-	} );
-
-	it( "requires password confirmation", function() {
-	} );
-
 	it( "requires email", function() {
+	} );
+
+	it( "validates email", function() {
 	} );
 
 	it( "requires email to be unique", function() {
 	} );
 
-	it( "validates email", function() {
+	it( "requires password", function() {
 	} );
 
 	it( "checks if a given password is valid", function() {
