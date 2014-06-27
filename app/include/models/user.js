f9ccec46ae45974f15d88f6b8e3cca7b3791b6be
twstddev <twstd.dev@gmail.com>
2014-06-27 10:57:55 +0000

    Add required, unique and email validations to the user model

--- app/include/models/user.js
+++ app/include/models/user.js
@@ -1,13 +1,49 @@
 var mongoose = require( "mongoose" );
+var unique_validator = require( "mongoose-unique-validator" );
+
+/**
+ * @brief Checks if the given value is an email.
+ * TODO: abstract this method out if needed.
+ * For this example it can be used here.
+ */
+var validate_email = function( value ) {
+	var email_regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+	return email_regex.test( value );
+}
 
 /**
  * @brief Implements user model.
  */
 var UserSchema = new mongoose.Schema( {
 	username : {
+		type : String,
+		required : true,
+		unique : true
+	},
+
+	email : {
+		type : String,
+		required : true,
+		validate : [ validate_email, "Provided email is invalid" ],
+		unique : true
+	},
+
+	password : {
+		type : String,
+		required : true
+	},
+
+	first_name : {
+		type : String
+	},
+
+	last_name : {
 		type : String
 	}
 } );
+
+UserSchema.plugin( unique_validator );
 
 try {
 	mongoose.model( "User", UserSchema );
