80a1f9de62c32d97fe99b738a55e66ca6091e71a
twstddev <twstd.dev@gmail.com>
2014-06-30 14:27:52 +0000

    Add user new form template

--- app/modules/admin/routes/users.js
+++ app/modules/admin/routes/users.js
@@ -1,6 +1,13 @@
 /**
  * @brief Displays a list of users.
  */
-exports.index = function( request, response ) {
+module.exports.index = function( request, response ) {
 	response.render( "admin/users/index" );
 };
+
+/**
+ * @brief Displays a new user form.
+ */
+module.exports.new = function( request, response ) {
+	response.render( "admin/users/new" );
+}
